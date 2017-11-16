0941672020419b611e5330a58e269d5ddbf396e6
yangli7 <yangli7@jd.com>
2017-11-16 15:09:44 +0800

    20171116学习归档

--- seconddemos/1115demo1.py
+++ seconddemos/1115demo1.py
@@ -3,5 +3,6 @@ print("I will now count my chickens:")
 p=25+30/6
 print("hens %d"%(p))
 p=100-25*3%4
-print("hens %d"%(100-25*3%4))
+print("hens %8.2f"%(100-25*3%4))
 print(3+2==2-7)
+print("5>=-3?",5>=-3)
